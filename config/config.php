ba72fcde8183e5851b38eb440a2efe87d36817f9
SocialWinnie <taiwan9028@gmail.com>
2019-08-13 13:44:34 +0800

    git pdo connection && pdo db config

--- config/config.php
+++ config/config.php
@@ -1,3 +1,14 @@
 <?php
 
-	
+	error_reporting (16);
+
+	define("dbchkConfig", serialize([
+		"mysql",
+		"192.168.0.171",
+		"3306",
+		"dbchk",
+		"haha",
+		"haha1",
+	]));
+
+?>
--- connection.php
+++ connection.php
@@ -4,7 +4,7 @@
 
 	use \PDO;
 
-	// require_once("../config/config.php");
+	require_once("./config/config.php");
 
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
@@ -20,27 +20,36 @@
 		private $userAccount;
 		private $userPassword;
 		private $dbWork;
-		private $data;
+		public $data;
 
-		function __construct()
+		function __construct($dbArray = [], $check = false)
 		{
 
-			// 宣告
-			$dbchkConfig = unserialize(dbchkConfig);
+			$dbConfig = (COUNT($dbArray) == 6)?$dbArray:unserialize(dbchkConfig);
 
-			// 初始化
-			$this->dbSelect     = $dbchkConfig['dbSelect'];
-			$this->dbHost       = $dbchkConfig['dbHost'];
-			$this->dbPort       = $dbchkConfig['dbPort'];
-			$this->dbName       = $dbchkConfig['dbName'];
-			$this->userAccount  = $dbchkConfig['userAccount'];
-			$this->userPassword = $dbchkConfig['userPassword'];
-
-			$dsn = "{$this->dbSelect}:host={$this->dbHost};port={$this->dbPort};dbname={$this->dbName}";
-
+			$this->dbSelect     = $dbConfig[0];
+			$this->dbHost       = $dbConfig[1];
+			$this->dbPort       = $dbConfig[2];
+			$this->dbName       = $dbConfig[3];
+			$this->userAccount  = $dbConfig[4];
+			$this->userPassword = $dbConfig[5];
+			
+			if ($check == false) {
+				$dsn = "{$this->dbSelect}:host={$this->dbHost};port={$this->dbPort};dbname={$this->dbName}";
+			} else {
+				$dsn = "{$this->dbSelect}:host={$this->dbHost};port={$this->dbPort};";
+			}
+			
 			try {
 
-				$this->dbWork = new PDO($dsn, $this->userAccount, $this->userPassword);
+				$this->dbWork = new PDO(
+					$dsn,
+					$this->userAccount,
+					$this->userPassword,
+					[
+						PDO::ATTR_TIMEOUT => 3,
+					]
+				);
 
 			} catch (PDOException $e) {
 
@@ -49,7 +58,6 @@
 			}
 
 			$this->dbWork->query('SET NAMES utf8');
-			// 指定PDO錯誤模式和錯誤處理
 			$this->dbWork->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 		}
@@ -61,11 +69,22 @@
 
 		}
 
-		public function db_select($sql)
+		public function db_select($sql, $status = 0)
 		{
+			switch ($status) {
+				case 1:
+					$pdoFetch = PDO::FETCH_NUM;
+					break;
+				case 2:
+				
+					break;
+				default:
+					$pdoFetch = PDO::FETCH_CLASS;
+					break;
+			}
 
 			$dbReady = $this->dbWork->query($sql);
-			$this->data = $dbReady->fetchAll(PDO::FETCH_CLASS);
+			$this->data = $dbReady->fetchAll($pdoFetch);
 
 			if ($this->data == false) {
 
@@ -79,7 +98,7 @@
 
 		public function db_update($sql)
 		{
-
+			
 			$dbReady = $this->dbWork->query($sql);
 			$this->data = $dbReady->execute();
 
@@ -92,7 +111,11 @@
 			return $this->data;
 
 		}
-
+			
 	}
-
+	
+	$test = new DbSet();
+	$sql = "SELECT * FROM `dbwhere` WHERE 1";
+	$data = $test->db_select($sql, 1);
+	print_r($data);
 ?>
